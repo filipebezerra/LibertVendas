58c59ab1cc213ab3af0f31209dbe56776952f10b
Filipe Bezerra <filipebzerra@gmail.com>
2016-09-22 22:19:15 -0300

    Template da home adicionado drawer.

--- app/src/main/java/br/com/libertsolutions/libertvendas/app/presentation/home/HomeActivity.java
+++ app/src/main/java/br/com/libertsolutions/libertvendas/app/presentation/home/HomeActivity.java
@@ -1,10 +1,37 @@
 package br.com.libertsolutions.libertvendas.app.presentation.home;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
 import br.com.libertsolutions.libertvendas.app.R;
 import br.com.libertsolutions.libertvendas.app.presentation.activity.LibertVendasActivity;
 
-public class HomeActivity extends LibertVendasActivity {
+/**
+ * @author Filipe Bezerra
+ */
+public class HomeActivity extends LibertVendasActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setAsHomeActivity();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, mToolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+    }
 
     @Override
     protected int provideContentViewResource() {
@@ -12,8 +39,35 @@ public class HomeActivity extends LibertVendasActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setAsHomeActivity();
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        item.setChecked(false);
+
+        if (id == R.id.nav_home) {
+
+        } else if (id == R.id.nav_pedidos) {
+
+        } else if (id == R.id.nav_clientes) {
+
+        } else if (id == R.id.nav_produtos) {
+
+        } else if (id == R.id.nav_settings) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
