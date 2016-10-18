74740dbfb6b05a46a14dd63a1e040a8cea0bbc25
Filipe Bezerra <filipebzerra@gmail.com>
2016-10-18 10:10:50 -0200

    Transformação da Fab numa SheetFab com animação.

--- app/src/main/java/br/com/libertsolutions/libertvendas/app/presentation/home/HomeActivity.java
+++ app/src/main/java/br/com/libertsolutions/libertvendas/app/presentation/home/HomeActivity.java
@@ -1,16 +1,22 @@
 package br.com.libertsolutions.libertvendas.app.presentation.home;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.CardView;
 import android.view.MenuItem;
+import android.view.View;
 import br.com.libertsolutions.libertvendas.app.R;
 import br.com.libertsolutions.libertvendas.app.presentation.activity.LibertVendasActivity;
+import br.com.libertsolutions.libertvendas.app.presentation.view.SheetFloatingActionButton;
+import butterknife.BindColor;
+import butterknife.BindView;
 import butterknife.OnClick;
+import com.gordonwong.materialsheetfab.DimOverlayFrameLayout;
+import com.gordonwong.materialsheetfab.MaterialSheetFab;
 
 /**
  * @author Filipe Bezerra
@@ -18,7 +24,16 @@ import butterknife.OnClick;
 public class HomeActivity extends LibertVendasActivity
         implements NavigationView.OnNavigationItemSelectedListener, HomeContract.View {
 
+    @BindView(R.id.fab) protected SheetFloatingActionButton mFloatingActionButton;
+    @BindView(R.id.fab_sheet) protected CardView mFabSheet;
+    @BindView(R.id.overlay) protected DimOverlayFrameLayout mFabOverlay;
+
+    @BindColor(R.color.background_card) protected int mFabSheetColor;
+    @BindColor(R.color.color_accent) protected int mAccentColor;
+
     private HomeContract.Presenter mPresenter;
+
+    private MaterialSheetFab<SheetFloatingActionButton> mMaterialSheetFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +52,9 @@ public class HomeActivity extends LibertVendasActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+
+        mMaterialSheetFab = new MaterialSheetFab<>(
+                mFloatingActionButton, mFabSheet, mFabOverlay, mFabSheetColor, mAccentColor);
     }
 
     @Override
@@ -49,6 +67,8 @@ public class HomeActivity extends LibertVendasActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        } else if (mMaterialSheetFab.isSheetVisible()) {
+            mMaterialSheetFab.hideSheet();
         } else {
             super.onBackPressed();
         }
@@ -97,13 +117,12 @@ public class HomeActivity extends LibertVendasActivity
         navigate().toPedidos(getSupportFragmentManager());
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @OnClick(R.id.fab)
-    void onFabClick() {
-        navigate().toPedido();
+    @OnClick({ R.id.fab_sheet_item_novo_pedido, R.id.fab_sheet_item_novo_cliente })
+    void onFabClick(View pView) {
+        if (pView.getId() == R.id.fab_sheet_item_novo_pedido) {
+            navigate().toPedido();
+        } else {
+            //navigate().toCliente();
+        }
     }
 }
