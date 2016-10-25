73ec9953623e4cb2c2403ebbed1eb51dde34263e
Filipe Bezerra <filipebzerra@gmail.com>
2016-10-25 09:06:01 -0200

    Inicialização do Realm.

---
+++ app/src/main/java/br/com/libertsolutions/libertvendas/app/LibertVendasApplication.java
@@ -0,0 +1,35 @@
+package br.com.libertsolutions.libertvendas.app;
+
+import android.app.Application;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import timber.log.Timber;
+
+/**
+ * @author Filipe Bezerra
+ */
+public class LibertVendasApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initializeLoggingOrCrashReporting();
+        initializeDataStorageWithRealm();
+    }
+
+    private void initializeLoggingOrCrashReporting() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            //TODO initialize CrashReportingTree integrated with Crashlytics
+        }
+    }
+
+    private void initializeDataStorageWithRealm() {
+        Realm.init(this);
+        RealmConfiguration configuration = new RealmConfiguration.Builder()
+                .name("libertapp.realm")
+                .schemaVersion(1)
+                .build();
+        Realm.setDefaultConfiguration(configuration);
+    }
+}
